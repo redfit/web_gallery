7ce4847f8b791cb92d35c680183e389fb363db53
fuchiwaki <fuchiwaki@kndb.jp>
2013-07-21 16:34:42 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebGallery::Application.config.secret_key_base = '7992b671636d5aa2761c15dc077d19a264e75ea796365dd04926a2a6158bc8f819a37d7abe2fae04e8efe4349d4046c72e3e69df606d8789f92f59be29c0eff9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebGallery::Application.config.session_store :cookie_store, key: '_web_gallery_session'
